269dcd878bf559fdfadebccc8ba135c0f6103b40
martineliasq <elias.quintero@udea.edu.co>
2020-05-21 19:32:42 -0500

    Jwt requets

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
 console.info("Load service script")
 console.info(axios)
-
+console.log(localStorage.getItem("token"))
 
 axios.get('https://random.dog/woof.json')
 .then(function (response) {
@@ -13,3 +13,35 @@ axios.get('https://random.dog/woof.json')
     let alerta = document.getElementById("alerta")
     alerta.style.display = "none"
 })
+
+
+
+
+
+let url = "http://161.35.110.128/api/v1/login/access-token"
+const requestBody = {
+  username: 'elias',
+  password: 'elias2019',
+
+}
+
+const config = {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+}
+
+axios({
+    method: 'post',
+    url: url,
+    data: Qs.stringify({
+      username: 'elias',
+      password: 'elias2019'
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+    }
+  }).then(response=>{
+      console.log(response)
+  })
+
